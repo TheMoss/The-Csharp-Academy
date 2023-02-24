9f38624ca43dee634cd479a3b76df751f6f3e043
TheMoss <milena.k98@gmail.com>
2023-02-24 18:06:58 +0100

    Initial commit

---
+++ HabitTracker/Program.cs
@@ -0,0 +1,18 @@
+﻿using HabitTracker;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+BsonClassMap.RegisterClassMap<HabitModel>();
+
+string connectionString = //connection string
+MongoClient dbClient = new MongoClient(connectionString);
+
+var habitsDatabase = dbClient.GetDatabase("HabitTracker");
+var habitsCollection = habitsDatabase.GetCollection<BsonDocument>("loggedHabits");
+
+var habitsApp = new HabitsAppUI() { MongoCollection = habitsCollection };
+while (habitsApp.IsRunning)
+{
+    habitsApp.MainMenu();
+}
