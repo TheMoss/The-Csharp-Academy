a11ed20ce3a446c3d199aadfd6ba73780ad931a4
TheMoss <milena.k98@gmail.com>
2023-02-25 13:50:00 +0100

    Add HabitModel class for mapping BSON documents.

---
+++ HabitTracker/HabitModel.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitTracker
+{
+    public class HabitModel
+    {        
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public int Quantity { get; set; }
+
+        public HabitModel(string date, int quantity, string name = "drinking water")
+        {
+            Name = name;
+            Date = date;
+            Quantity = quantity;
+        }
+    }
+}
