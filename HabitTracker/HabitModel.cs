18693d761c0004fd123c0d7e561def9ba794ae17
TheMoss <milena.k98@gmail.com>
2023-03-13 18:19:25 +0100

    Add BsonElement attributes to corresponding HabitModel properties.

--- HabitTracker/HabitModel.cs
+++ HabitTracker/HabitModel.cs
@@ -1,4 +1,6 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +11,16 @@ namespace HabitTracker
 {
     public class HabitModel
     {        
+        public ObjectId Id { get; set; }        
+        [BsonElement("name")]
         public string Name { get; set; }
+        [BsonElement("date")]
         public string Date { get; set; }
+        [BsonElement("quantity")]
         public int Quantity { get; set; }
 
         public HabitModel(string date, int quantity, string name = "drinking water")
-        {
+        {            
             Name = name;
             Date = date;
             Quantity = quantity;
